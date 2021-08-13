ec1f7348e98f9c69d531ad587f49648abfc8e559
Philip Laxamana <philip.laxamana@hotmail.com>
2021-08-12 23:02:32 -0400

    adding preview

---
+++ pages/api/exit-preview.js
@@ -0,0 +1,5 @@
+export default function exitPreview(req, res) {
+  res.clearPreviewData()
+  res.writeHead(307, { Location: '/' })
+  res.end()
+}
---
+++ pages/api/preview.js
@@ -0,0 +1,41 @@
+import { getPostBySlug, getProjectBySlug, } from '@/lib/api'
+
+export async function enablePreviewPost(req, res) {
+  if (
+    req.query.secret !== process.env.SANITY_PREVIEW_SECRET ||
+    !req.query.slug
+  ) {
+    return res.status(401).json({ message: 'invalid token' })
+  }
+
+  const blog = await getPostBySlug(req.query.slug, true)
+  if (!blog) {
+    return res.status(401).json({ message: 'invalid slug' })
+  }
+
+  // setPreviewData will set cookies into your browser
+  // __prerender_bypass __next_preview_data
+  res.setPreviewData({})
+  res.writeHead(307, { Location: `/blog/${blog.slug}` })
+  res.end()
+}
+
+export async function enablePreviewProject(req, res) {
+  if (
+    req.query.secret !== process.env.SANITY_PREVIEW_SECRET ||
+    !req.query.slug
+  ) {
+    return res.status(401).json({ message: 'invalid token' })
+  }
+
+  const project = await getProjectBySlug(req.query.slug, true)
+  if (!project) {
+    return res.status(401).json({ message: 'invalid slug' })
+  }
+
+  // setPreviewData will set cookies into your browser
+  // __prerender_bypass __next_preview_data
+  res.setPreviewData({})
+  res.writeHead(307, { Location: `/portfolio/${project.slug}` })
+  res.end()
+}
