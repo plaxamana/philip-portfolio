d37d61df8839663c7fb605e9b0c12684ec947305
Philip Laxamana <philip.laxamana@hotmail.com>
2021-08-14 12:54:12 -0400

    changed alert style to opacity-90

--- components/Alert.js
+++ components/Alert.js
@@ -5,7 +5,7 @@ export default function Alert({ message, closeAlert, setCloseAlert }) {
     <div
       className={`${
         closeAlert ? 'hidden' : null
-      } z-50 absolute top-0 left-0 right-0 flex items-center justify-center w-full p-4 mx-auto text-lg text-center text-white bg-blue-700 border-2 border-blue-900 `}
+      } z-50 absolute top-0 left-0 right-0 flex items-center justify-center w-full p-4 mx-auto text-lg text-center text-white bg-blue-700 bg-opacity-90 `}
     >
       <p className='mr-2'>{message}</p>
       <FiX className='w-6 h-6 cursor-pointer' onClick={setCloseAlert} />
